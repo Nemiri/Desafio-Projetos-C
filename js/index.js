6b67d28e6e8d1d7090ab34036f15d6bcdf7beb7e
Ian Langkammer Batista <ilangkammer16@gmail.com>
2021-04-15 21:22:45 -0300

    navbar effects on, scroll effect apllied but need some fixing  and the home page is already responsive too

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/* Custom Javascript */
+
+/* Mouse Scroll Active Link Navbar */
+$(function() {
+    var links = $("#navbarNav a");
+
+    $(window).scroll(function() {
+        var topScroll = $(window).scrollTop();
+        links.each(function() {
+            var href = $(this).attr('href');
+            var el = $(href);
+            var posSection = el.offset().top;
+            var hSection = el.height();
+            
+            if(posSection <= topScroll && (posSection + hSection) > topScroll){
+                links.removeClass('active')
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active');
+            }
+        });
+    });
+});
